783da5bd799a637be9ecf378cda436359ffed84e
Kishan Patel <kishan02patel@gmail.com>
2019-12-11 01:18:48 +1100

    Add login validation function tests and reformat

--- helpers/validations/login.js
+++ helpers/validations/login.js
@@ -1,27 +1,25 @@
-const validator = require("validator");
-const { isEmpty } = require("./custom");
+const validator = require('validator');
+const { isEmpty } = require('./custom');
 
-const validateLoginInput = data => {
-	let errors = {};
+const validateLoginInput = (userInput) => {
+  const errors = {};
+  const data = userInput;
 
-	//Call our own isEmpty function to check whether the field have values or not. This is done because the validator fucntion takes only strings as as input. So if value is not defined then it will not work.
+  // Call our own isEmpty function to check whether the field have values or not. This is done because the validator fucntion takes only strings as as input. So if value is not defined then it will not work.
 
-	data.email = !isEmpty(data.email) ? data.email : "";
-	data.password = !isEmpty(data.password) ? data.password : "";
+  data.email = !isEmpty(data.email) ? data.email : '';
+  data.password = !isEmpty(data.password) ? data.password : '';
 
-	if (!validator.isEmail(data.email))
-		errors.email = "Invalid email address";
+  if (!validator.isEmail(data.email)) { errors.email = 'Invalid email address'; }
 
-	if (validator.isEmpty(data.email))
-		errors.email = "Email field cannot be empty";
+  if (validator.isEmpty(data.email)) { errors.email = 'Email field cannot be empty'; }
 
-	if (validator.isEmpty(data.password))
-		errors.password = "Password field cannot be empty";
+  if (validator.isEmpty(data.password)) { errors.password = 'Password field cannot be empty'; }
 
-	return {
-		errors,
-		isValid: isEmpty(errors)
-	};
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
 };
 
 module.exports = validateLoginInput;
